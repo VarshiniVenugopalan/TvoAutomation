c45804f118aba9e82e22b36974c6ede7bb962fad
Varshini Venugopalan <vvarshinigopalan@gmail.com>
2023-06-13 14:28:35 -0400

    Updated config.js and package.json to fix issue

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'h3m4v4',
+  projectId: 't23bkt',
   e2e: {
     setupNodeEvents(on, config) {
-     // projectId: "dce93661-4ed8-49a9-8459-05ec7dae57ab"
-      // implement node event listeners here
-
     },
   },
 });
