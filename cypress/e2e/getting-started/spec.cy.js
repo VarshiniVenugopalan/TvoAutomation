ecf44dddc209ba6e76b36753bbd3fc047aa56740
Varshini Venugopalan <136201968+VarshiniVenugopalan@users.noreply.github.com>
2023-06-14 21:54:38 -0400

    Update spec.cy.js

--- cypress/e2e/getting-started/spec.cy.js
+++ cypress/e2e/getting-started/spec.cy.js
@@ -1,11 +1,10 @@
 describe('My Simple Test', () => {
   it('Visit TVO Learn', () => {
-   // expect(true).to.equal(false)
     cy.visit('https://tvolearn.com/')
     cy.get('.medium-up-hide-modified > .site-header__icons-wrapper > .site-header__menu').click()
     cy.get(':nth-child(1) > .mobile-nav__link').click()
     cy.get(':nth-child(1) > .mobile-nav__dropdown > :nth-child(6) > .mobile-nav__sublist-link').click()
-    // Wait for an element with the selector to be visible for a maximum of 5 seconds
+    // Wait for an element  to be visible for a maximum of 5 seconds
     cy.contains('Mathematics', { timeout: 5000 }).should('be.visible');
     cy.contains('Mathematics').click()
   });
